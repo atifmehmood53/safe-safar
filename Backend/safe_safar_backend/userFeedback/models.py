04a810716037febae28421f4796c7846c7c990ae
noor <noor@stellic.com>
2021-09-18 16:22:07 +0500

    Added models definition

--- Backend/safe_safar_backend/userFeedback/models.py
+++ Backend/safe_safar_backend/userFeedback/models.py
@@ -1,12 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 class Ride(models.Model):
     pass
 
-class UserRide(models.Model):
-    pass
 
-class Feedback(models.Model):
-    pass
+class UserRide(models.Model):
+    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    ride = models.ForeignKey(Ride, unique=True, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=10)
+
+
+class RidePalFeedback(models.Model):
+    user_ride = models.ForeignKey(UserRide, on_delete=models.CASCADE)
+    pal_ride = models.ForeignKey(UserRide, on_delete=models.CASCADE)
+
+    NEUTRAL = 'neutral'
+    DOWN = 'down'
+    UP = 'up'
+    BLACKLIST = 'blacklist'
+
+    VOTE_CHOICES = (
+        (NEUTRAL, 'neutral'),
+        (DOWN, 'down'),
+        (UP, 'up'),
+        (BLACKLIST, 'blacklist')
+    )
+    vote = models.CharField(max_length=10, choices=VOTE_CHOICES, default=NEUTRAL)
+
+    class Meta:
+        unique_together = ("user_ride", "pal_ride")
 
