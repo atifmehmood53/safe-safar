29a5db10693c2c949c9e8a88c169965279d8b0c9
Taban Shaukat <taban@retrocausal.ai>
2021-09-18 15:28:50 +0500

    Added userFeedback and UserPreference apps and urlpatterns

---
+++ Backend/safe_safar_backend/userFeedback/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Ride(models.Model):
+    pass
+
+class UserRide(models.Model):
+    pass
+
+class Feedback(models.Model):
+    pass
+
---
+++ Backend/safe_safar_backend/userPreference/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    pass
+class UserPreference(models.Model):
+    pass
+class Preference(models.Model):
+    pass
+
+class AnswerOptions(models.Model):
+    pass
