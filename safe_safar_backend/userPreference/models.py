f99858bcd5dd84d7abef0337a5fcdb61043488d9
ReebaAslam <reebaaslam23@gmail.com>
2021-09-18 17:25:15 +0500

    user preference models

--- safe_safar_backend/userPreference/models.py
+++ safe_safar_backend/userPreference/models.py
@@ -1,12 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    pass
-class UserPreference(models.Model):
-    pass
-class Preference(models.Model):
-    pass
 
-class AnswerOptions(models.Model):
-    pass
+# Create your models here.
+class User(User):
+    community_score = models.FloatField()
+
+
+class UserPreferenceAnswer(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    preference_answers = models.ManyToManyField("PreferenceAnswer")
+
+
+class PreferenceQuestion(models.Model):
+    question = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+
+
+class PreferenceAnswer(models.Model):
+    preference_question = models.ForeignKey("PreferenceQuestion", on_delete=models.CASCADE)
+    answer = models.CharField(max_length=10)
+
+    class Meta:
+        unique_together = ['preference_question', 'answer']
