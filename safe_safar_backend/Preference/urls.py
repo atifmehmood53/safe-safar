4ed63dd6de9c218682c67c7decff08e6e2e96920
noor <noor@stellic.com>
2021-09-19 11:44:55 +0500

    seializers, views, urls

--- safe_safar_backend/Preference/urls.py
+++ safe_safar_backend/Preference/urls.py
@@ -1,12 +1,15 @@
 
 from django.urls import path, include
 
-from .views import ListCreateCustomerPreference, GetCustomerPreference
+import Preference.views
+# from .views import ListCreateCustomerPreference, GetCustomerPreference
 
 app_name = "Preference"
 
 urlpatterns = [
-    path('', ListCreateCustomerPreference.as_view(), name='create_preference'),
-    path('<int:id>', GetCustomerPreference.as_view(), name='retreive_preference'),
+    # path('', ListCreateCustomerPreference.as_view(), name='create_preference'),
+    # path('<int:id>', GetCustomerPreference.as_view(), name='retreive_preference'),
     # path('user_preference/', include('Preference.urls', "Preference")),
+
+    path(r'^preference_questions/$', Preference.views.get_all_questions_with_options, name='preference_questions'),
 ]
--- safe_safar_backend/Preference/views.py
+++ safe_safar_backend/Preference/views.py
@@ -1,8 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 
-from .models import CustomerPreferenceAnswer
+from .models import CustomerPreferenceAnswer, PreferenceQuestion
 from .serializers import UserPreferenceSerializer
 
 class ListCreateCustomerPreference(generics.CreateAPIView):
@@ -13,3 +14,8 @@ class GetCustomerPreference(generics.RetrieveAPIView):
     queryset = CustomerPreferenceAnswer.objects.all()
     serializer_class = UserPreferenceSerializer
     lookup_field = "id"
+
+
+def get_all_questions_with_options(request):
+    all_questions = [q.serialize() for q in PreferenceQuestion.objects.all()]
+    return JsonResponse(all_questions)
