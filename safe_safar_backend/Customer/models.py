2f1ab40ff02d239ae2f9432ad6649561398cd3ec
noor <noor@stellic.com>
2021-09-19 10:19:55 +0500

    added helpers, changed some models, new script for populate data

--- safe_safar_backend/Customer/models.py
+++ safe_safar_backend/Customer/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class Customer(User):
     community_score = models.FloatField()
     seats = models.ManyToManyField("RideFeedback.Seat", through="RideFeedback.Booking")
+
+    def update_community_score(self, all_pfs):
+        all_pfs_count = all_pfs.count()
+        if all_pfs_count < 5:
+            return
+        self.community_score = sum(map(lambda x: float(x), all_pfs.values_list('vote', flat=True))) / all_pfs_count
+        self.save(update_fields=['community_score'])
--- safe_safar_backend/RideFeedback/models.py
+++ safe_safar_backend/RideFeedback/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 
@@ -14,23 +16,40 @@ class Seat(models.Model):
 class Booking(models.Model):
     customer = models.ForeignKey("Customer.Customer", on_delete=models.CASCADE)
     seat = models.ForeignKey("Seat", on_delete=models.CASCADE)
+    # datetime
+
+    class Meta:
+        # TODO: Unique with datetime
+        unique_together = ("customer", "seat")
+
+    def provide_feedback_to_booking(self, another_booking, vote):
+        pf = PalFeedback.objects.create(booking=self, pal_booking=another_booking, vote=vote)
+        cms, _ = CustomersMatchScore.objects.get_or_create(pal_1=self.customer, pal_2=another_booking.customer)
+        cms.update_score_with_vote(pf.vote)
+        another_booking.update_community_score()
+
+    def update_community_score(self):
+        self.customer.update_community_score(self.pal_feedback.all())
 
 
 class CustomersMatchScore(models.Model):
-    pal1 = models.ForeignKey("Customer.Customer", on_delete=models.CASCADE)
-    pal2 = models.ForeignKey("Customer.Customer", on_delete=models.CASCADE, related_name='pal2')
-    score1 = models.DecimalField(decimal_places=3, max_digits=7)  # pal 1 scores pal 2
-    score2 = models.DecimalField(decimal_places=3, max_digits=7)  # pal 2 scores pal 1
+    pal_1 = models.ForeignKey("Customer.Customer", on_delete=models.CASCADE)
+    pal_2 = models.ForeignKey("Customer.Customer", on_delete=models.CASCADE, related_name='pal2')
+    score = models.DecimalField(decimal_places=3, max_digits=7, default=Decimal('1.000'))  # pal 1 scores pal 2
+
+    def update_score_with_vote(self, vote):
+        self.score *= Decimal(vote)
+        self.save(update_fields=['score'])
 
 
 class PalFeedback(models.Model):
     booking = models.ForeignKey("Booking", on_delete=models.CASCADE)
-    pal_booking = models.ForeignKey("Booking", on_delete=models.CASCADE, related_name='pal_booking')
+    pal_booking = models.ForeignKey("Booking", on_delete=models.CASCADE, related_name='pal_feedback')
 
-    NEUTRAL = 'neutral'
-    DOWN = 'down'
-    UP = 'up'
-    BLACKLIST = 'blacklist'
+    NEUTRAL = '1.0'
+    DOWN = '0.8'
+    UP = '1.3'
+    BLACKLIST = '0.0'
 
     VOTE_CHOICES = (
         (NEUTRAL, 'neutral'),
