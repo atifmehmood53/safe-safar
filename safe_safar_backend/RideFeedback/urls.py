8116f4cdccd2d0a2932f1e25b1b2a149232374bd
noor <noor@stellic.com>
2021-09-19 14:17:11 +0500

    fixed typo

--- safe_safar_backend/RideFeedback/urls.py
+++ safe_safar_backend/RideFeedback/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
+
+# from .views import ListCreateCustomerPreferenceAnswer
+import RideFeedback.views
+
+
 app_name = "RideFeedback"
+
+urlpatterns = [
+    path('submit_feedback', RideFeedback.views.submit_feedback, name='submit_feedback'),
+    path('get_voting_options', RideFeedback.views.get_voting_options, name='get_voting_options'),
+]
